36bc3ba0bacd2c1bd2af247262b4096639add19e
Oleksandr Nazarenko <jaroros@gmail.com>
2020-05-13 18:47:52 +0300

    refactored page url to properties

--- src/main/java/me/bill/currencyparser/exceptions/CurrencyCodeNotValid.java
+++
@@ -1,8 +0,0 @@
-package me.bill.currencyparser.exceptions;
-
-public class CurrencyCodeNotValid extends RuntimeException {
-
-    public CurrencyCodeNotValid(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
--- src/main/java/me/bill/currencyparser/service/CurrencyPageLoader.java
+++ src/main/java/me/bill/currencyparser/service/CurrencyPageLoader.java
@@ -2,11 +2,13 @@ package me.bill.currencyparser.service;
 
 import lombok.SneakyThrows;
 import org.jsoup.helper.HttpConnection;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CurrencyPageLoader {
-    public static final String PAGE_URL = "https://en.wikipedia.org/wiki/ISO_4217";
+    @Value("${currency.source.url}")
+    public String PAGE_URL;
 
     @SneakyThrows
     public String loadCurrencyPage() {

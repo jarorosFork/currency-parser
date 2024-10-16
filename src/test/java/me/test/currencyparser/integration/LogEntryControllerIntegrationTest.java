6173db12426d47429738cebce13c1cad4c52e268
Oleksandr Nazar <jaroros@gmail.com>
2024-10-16 18:44:14 +0300

    Add REST API to update LogEntry username by id
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/jarorosFork/currency-parser/tree/update-java-gradle?shareId=XXXX-XXXX-XXXX-XXXX).

--- src/test/java/me/test/currencyparser/integration/LogEntryControllerIntegrationTest.java
+++ src/test/java/me/test/currencyparser/integration/LogEntryControllerIntegrationTest.java
@@ -12,6 +12,7 @@ import org.springframework.web.servlet.ModelAndView;
 import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class LogEntryControllerIntegrationTest extends BaseIntegrationTest {
@@ -32,5 +33,25 @@ public class LogEntryControllerIntegrationTest extends BaseIntegrationTest {
         List<LogEntry> logEntryList = (List<LogEntry>) modelAndViewResult.getModel().get("logEntries");
         Assertions.assertEquals("list", modelAndViewResult.getViewName());
         Assertions.assertEquals(CODE, logEntryList.get(0).getCurrencyCode());
+    }
+
+    @Test
+    void shouldUpdateLogEntryUsername() throws Exception {
+        logEntryRepository.deleteAll(logEntryRepository.findAll());
+        mvc.perform(get(CurrencyController.URL.replace("{code}", CODE))
+                .contentType("application/json"))
+                .andExpect(status().isOk()).andReturn().getResponse().getContentAsString();
+
+        ModelAndView modelAndViewResult = mvc.perform(get(LogEntryController.URL))
+                .andExpect(status().isOk()).andReturn().getModelAndView();
+        List<LogEntry> logEntryList = (List<LogEntry>) modelAndViewResult.getModel().get("logEntries");
+        Long logEntryId = logEntryList.get(0).getId();
+
+        mvc.perform(put("/log/" + logEntryId + "/username")
+                .param("username", "newUsername"))
+                .andExpect(status().isOk());
+
+        LogEntry updatedLogEntry = logEntryRepository.findById(logEntryId).orElseThrow();
+        Assertions.assertEquals("newUsername", updatedLogEntry.getUsername());
     }
 }
